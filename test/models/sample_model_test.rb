71145682bd3810356678bee70fc998c5598a878c
moch.alamsyah <moch.alamsyah@go-jek.com>
2019-12-02 11:14:13 +0530

    Add save model inside transaction

--- test/models/sample_model_test.rb
+++ test/models/sample_model_test.rb
@@ -3,11 +3,18 @@ require 'test_helper'
 class SampleModelTest < ActiveSupport::TestCase
   test "should save sample model with id from database" do
     sample_model = SampleModel.new(age: 23, name: "Super awesome name")
-    puts sample_model.id
     sample_model.second_sample_models << SecondSampleModel.new(phone: "+65123", sample_model: sample_model)
-    sample_model.save
+
+    sample_model.transaction do
+      if sample_model.save
+        puts "Saving"
+      end
+      sample_model
+    end
+
     puts sample_model.id
     puts sample_model.second_sample_models.first.sample_model_id
+
     assert_equal sample_model.id, SampleModel.all.last.id, "id should be equal"
   end
 end
